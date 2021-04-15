b879c28ba426fdde62e8ec501d7c58c718653a8b
Lucas Di Stasi <distasilucas@gmail.com>
2021-04-14 22:10:22 -0300

    Added Footer links

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -16,61 +16,43 @@ export const FooterComponent = () => {
           <div className="mt-8 sm:mt-0 sm:w-full sm:px-8 flex flex-col md:flex-row justify-between">
             <div className="flex flex-col">
               <span className="font-bold text-gray-700 uppercase mb-2">
-                Footer header 1
+                My Network
               </span>
               <span className="my-2">
-                <a href="#" className="text-blue-700  text-md hover:text-blue-500">
-                  link 1
+                <a href="https://www.linkedin.com/in/lucas-david-distasi/"
+                   className="text-blue-700  text-md hover:text-blue-500"
+                   target="_blank"
+                   rel="noreferrer">
+                  Linkedin
                 </a>
               </span>
               <span className="my-2">
-                <a href="#" className="text-blue-700  text-md hover:text-blue-500">
-                  link 1
-                </a>
-              </span>
-              <span className="my-2">
-                <a href="#" className="text-blue-700  text-md hover:text-blue-500">
-                  link 1
-                </a>
-              </span>
-            </div>
-            <div className="flex flex-col">
-              <span className="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">
-                Footer header 2
-              </span>
-              <span className="my-2">
-                <a href="#" className="text-blue-700 text-md hover:text-blue-500">
-                  link 1
-                </a>
-              </span>
-              <span className="my-2">
-                <a href="#" className="text-blue-700  text-md hover:text-blue-500">
-                  link 1
-                </a>
-              </span>
-              <span className="my-2">
-                <a href="#" className="text-blue-700 text-md hover:text-blue-500">
-                  link 1
+                <a href="https://gitlab.com/lucas.distasi"
+                   className="text-blue-700  text-md hover:text-blue-500"
+                   target="_blank"
+                   rel="noreferrer">
+                  GitLab
                 </a>
               </span>
             </div>
             <div className="flex flex-col">
               <span className="font-bold text-gray-700 uppercase mt-4 md:mt-0 mb-2">
-                Footer header 3
+                TMDB
               </span>
               <span className="my-2">
-                <a href="#" className="text-blue-700  text-md hover:text-blue-500">
-                  link 1
+                <a href="https://www.themoviedb.org/"
+                   className="text-blue-700  text-md hover:text-blue-500"
+                   target="_blank"
+                   rel="noreferrer">
+                  TMDB Site
                 </a>
               </span>
               <span className="my-2">
-                <a href="#" className="text-blue-700 text-md hover:text-blue-500">
-                  link 1
-                </a>
-              </span>
-              <span className="my-2">
-                <a href="#" className="text-blue-700  text-md hover:text-blue-500">
-                  link 1
+                <a href="https://developers.themoviedb.org/4/getting-started/authorization#"
+                   className="text-blue-700 text-md hover:text-blue-500"
+                   target="_blank"
+                   rel="noreferrer">
+                  TMDB API
                 </a>
               </span>
             </div>
@@ -80,12 +62,18 @@ export const FooterComponent = () => {
       <div className="container mx-auto px-6">
         <div className="mt-16 border-t-2 border-gray-300 flex flex-col items-center">
           <div className="sm:w-2/3 text-center my-4">
-            <a href="https://www.linkedin.com/in/lucas-david-distasi/" className="text-sm text-blue-700 font-bold mb-2" target="_blank">
+            <a href="https://www.linkedin.com/in/lucas-david-distasi/"
+               className="text-sm text-blue-700 font-bold mb-2"
+               target="_blank"
+               rel="noreferrer">
               2021 - By Lucas Di Stasi
             </a>
           </div>
           <div className="sm:w-2/3 text-center mb-6">
-            <a href="https://www.themoviedb.org/" className="text-sm text-blue-700 font-bold mb-2" target="_blank">
+            <a href="https://www.themoviedb.org/"
+               className="text-sm text-blue-700 font-bold mb-2"
+               target="_blank"
+               rel="noreferrer">
               Powered by The Movie DB
             </a>
           </div>
