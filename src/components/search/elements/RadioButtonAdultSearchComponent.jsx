ed1c3902b8532f8a0a7b32c3b1c2d945f6c798d0
Lucas Di Stasi <distasilucas@gmail.com>
2021-04-21 08:52:25 -0300

    Added sweet alert modal confirmation for +18 searchs

--- src/components/search/elements/RadioButtonAdultSearchComponent.jsx
+++ src/components/search/elements/RadioButtonAdultSearchComponent.jsx
@@ -1,4 +1,28 @@
+import Swal from "sweetalert2";
+
 export const RadioButtonAdultSearchComponent = () => {
+
+  const askForConfirmation = (e) => {
+    if (e.target.checked) {
+      Swal.fire({
+        title: 'Are you sure?',
+        text: "This might include +18 adult searches results",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes',
+        showClass: {
+          popup: 'animate__animated animate__flipInX'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__flipOutY'
+        }
+      }).then((result) => {
+        e.target.checked = result.isConfirmed
+      })
+    }
+  }
 
   return (
     <div className="w-full flex justify-between">
@@ -11,7 +35,8 @@ export const RadioButtonAdultSearchComponent = () => {
                name="toggle"
                id="adult-only"
                className="checked:bg-blue-500 outline-none focus:outline-none right-4 checked:right-0 duration-200
-                ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"/>
+                ease-in absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+                onClick={askForConfirmation}/>
         <label htmlFor="adult-only"
                className="block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer">
         </label>
