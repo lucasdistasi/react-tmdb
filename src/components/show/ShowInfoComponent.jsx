34bf72351b45879511fff44698e8575932b42362
Lucas Di Stasi <distasilucas@gmail.com>
2021-05-02 21:38:33 -0300

    Added a spinner to display while the show info is being retrieved

--- src/components/show/ShowInfoComponent.jsx
+++ src/components/show/ShowInfoComponent.jsx
@@ -35,88 +35,87 @@ export const ShowInfoComponent = () => {
   })
 
   return (
-    loading ? <SpinnerComponent color={"#EA01FFFF"}/> : _error ? <ErrorComponent/> :
-      <div>
-        { info &&
-          <>
-            <div
-              className="animate__animated animate__fadeIn animate__delay-1s parallax mid-parallax flex flex-col items-center justify-center"
-              style={{
-                backgroundImage: `url(${info.backdrop_path ?
-                  getPosterPath("original", info.backdrop_path) :
-                  NO_POSTER})`
-              }}/>
+    loading ?
+      <SpinnerComponent color={"#EA01FFFF"}/> :
+      _error ? <ErrorComponent/> : info ?
+        <div>
+          <div
+            className="animate__animated animate__fadeIn animate__delay-1s parallax mid-parallax flex flex-col items-center justify-center"
+            style={{
+              backgroundImage: `url(${info.backdrop_path ?
+                getPosterPath("original", info.backdrop_path) :
+                NO_POSTER})`
+            }}/>
 
-            <div className="container mx-auto bg-gray-50">
-              <div className="py-12">
-                <div className="text-center">
-                  <h1 className="text-7xl py-6">{info.name}</h1>
-                  <TaglineComponent tagline={info.tagline}/>
-                  <TrailerComponent trailers={trailers} elementId={info.id} elementType="tv"/>
-                  <p className="text-lg mx-28 mt-12 pb-8">{info.overview}</p>
-                </div>
-                <div className="flex flex-col flex-wrap lg:flex-row items-center lg:justify-between mx-16 mt-10">
-                  <InfoIconComponent info={info.vote_average}
-                                     icon={faStar}
-                                     color="text-yellow-500"
-                                     title="Rating"/>
-                  <InfoIconComponent info={info.first_air_date}
-                                     icon={faCalendarAlt}
-                                     color="text-green-500"
-                                     title="First Air Date"/>
-                  <InfoIconComponent info={info.last_air_date}
-                                     icon={faCalendarAlt}
-                                     color="text-blue-500"
-                                     title="Last Air Date"/>
-                  <InfoIconComponent info={info.number_of_seasons}
-                                     icon={faVideo}
-                                     color="text-indigo-500"
-                                     title="Seasons"/>
-                  <InfoIconComponent info={info.number_of_episodes}
-                                     icon={faFilm}
-                                     color="text-purple-500"
-                                     title="Total Episodes"/>
-                </div>
+          <div className="container mx-auto bg-gray-50">
+            <div className="py-12">
+              <div className="text-center">
+                <h1 className="text-7xl py-6">{info.name}</h1>
+                <TaglineComponent tagline={info.tagline}/>
+                <TrailerComponent trailers={trailers} elementId={info.id} elementType="tv"/>
+                <p className="text-lg mx-28 mt-12 pb-8">{info.overview}</p>
               </div>
-
-              <div className="py-14">
-                <div className="flex flex-col lg:flex-row items-center lg:justify-around mx-16 mt-5">
-                  <GenreComponent genres={info.genres}/>
-                  <ProductionCompaniesComponent productionCompanies={info.production_companies}/>
-                  <StatusComponent status={info.status}/>
-                  <HomePageComponent homepage={info.homepage} title={info.name}/>
-                </div>
+              <div className="flex flex-col flex-wrap lg:flex-row items-center lg:justify-between mx-16 mt-10">
+                <InfoIconComponent info={info.vote_average}
+                                   icon={faStar}
+                                   color="text-yellow-500"
+                                   title="Rating"/>
+                <InfoIconComponent info={info.first_air_date}
+                                   icon={faCalendarAlt}
+                                   color="text-green-500"
+                                   title="First Air Date"/>
+                <InfoIconComponent info={info.last_air_date}
+                                   icon={faCalendarAlt}
+                                   color="text-blue-500"
+                                   title="Last Air Date"/>
+                <InfoIconComponent info={info.number_of_seasons}
+                                   icon={faVideo}
+                                   color="text-indigo-500"
+                                   title="Seasons"/>
+                <InfoIconComponent info={info.number_of_episodes}
+                                   icon={faFilm}
+                                   color="text-purple-500"
+                                   title="Total Episodes"/>
               </div>
-
-              <PeopleGridComponent
-                people={filterDuplicatedElements(info.created_by)}
-                title={"Created by"}
-                loadMoreFunction={null}
-                isLoading={loading}
-                currentPage={0}
-                totalPages={0}/>
-
-              <NetworkGridComponent networks={info.networks}
-                                    title={"Networks"}/>
-
-              <NetworkGridComponent networks={info.production_companies}
-                                    title={"Production Companies"}/>
-
-              {
-                info.seasons && <SeasonAccordionComponent seasons={info.seasons} showId={info.id} originalPoster={info.backdrop_path}/>
-              }
-
-              <ReviewsGridComponent reviews={reviews}  isLoading={loadingReviews} hasErrors={_errorReviews}/>
-
-              <CatalogueGridComponent elements={similarMovies.elements}
-                                      isLoading={similarLoading}
-                                      currentPage={similarMovies.currentPage}
-                                      totalPages={similarMovies.totalPages}
-                                      title="Similar shows"
-                                      elementType="shows"/>
             </div>
-          </>
-        }
-      </div>
+
+            <div className="py-14">
+              <div className="flex flex-col lg:flex-row items-center lg:justify-around mx-16 mt-5">
+                <GenreComponent genres={info.genres}/>
+                <ProductionCompaniesComponent productionCompanies={info.production_companies}/>
+                <StatusComponent status={info.status}/>
+                <HomePageComponent homepage={info.homepage} title={info.name}/>
+              </div>
+            </div>
+
+            <PeopleGridComponent
+              people={filterDuplicatedElements(info.created_by)}
+              title={"Created by"}
+              loadMoreFunction={null}
+              isLoading={loading}
+              currentPage={0}
+              totalPages={0}/>
+
+            <NetworkGridComponent networks={info.networks}
+                                  title={"Networks"}/>
+
+            <NetworkGridComponent networks={info.production_companies}
+                                  title={"Production Companies"}/>
+
+            {
+              info.seasons &&
+              <SeasonAccordionComponent seasons={info.seasons} showId={info.id} originalPoster={info.backdrop_path}/>
+            }
+
+            <ReviewsGridComponent reviews={reviews} isLoading={loadingReviews} hasErrors={_errorReviews}/>
+
+            <CatalogueGridComponent elements={similarMovies.elements}
+                                    isLoading={similarLoading}
+                                    currentPage={similarMovies.currentPage}
+                                    totalPages={similarMovies.totalPages}
+                                    title="Similar shows"
+                                    elementType="shows"/>
+          </div>
+        </div> : <SpinnerComponent color={"#EA01FFFF"}/>
   )
 }
